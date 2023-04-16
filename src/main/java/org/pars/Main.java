bb87caa30f45ebdac0b4c2cba32082db698f8ae6
Ikohido <rus.kanpaev@mail.ru>
2023-04-16 19:03:51 +0300

    Third

--- src/main/java/org/pars/Main.java
+++ src/main/java/org/pars/Main.java
@@ -11,16 +11,20 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ArrayList list = new ArrayList();
-
         Document document = Jsoup.connect("https://readmanga.live/list?sortType=rate")
                 .userAgent("Chrome/81.0.4044.138").get();
 
         Elements allAnime = document.getElementsByClass("col-sm-6");
-        Elements img = allAnime.select("img");
-        for (Element element : img) {
-            System.out.println(element.getElementsByAttribute("title"));
+        Elements img = allAnime.first().getElementsByClass("img");
+        Elements hover = img.first().getElementsByClass("non-hover");
+        hover.select("img.class");
+        for (Element element : hover) {
+            System.out.println(element.childNode(1).attr("title"));
+            System.out.println(element.childNode(1).attr("data-original"));
+
         }
 
     }
 }
+
+
