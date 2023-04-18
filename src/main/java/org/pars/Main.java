b14201596ddfabaf38cde591e64a175721ef0fd8
Ikohido <rus.kanpaev@mail.ru>
2023-04-18 16:28:36 +0300

    Four

--- src/main/java/org/pars/Main.java
+++ src/main/java/org/pars/Main.java
@@ -15,14 +15,23 @@ public class Main {
                 .userAgent("Chrome/81.0.4044.138").get();
 
         Elements allAnime = document.getElementsByClass("col-sm-6");
-        Elements img = allAnime.first().getElementsByClass("img");
-        Elements hover = img.first().getElementsByClass("non-hover");
-        hover.select("img.class");
-        for (Element element : hover) {
-            System.out.println(element.childNode(1).attr("title"));
-            System.out.println(element.childNode(1).attr("data-original"));
+        Elements imgAndTitle = allAnime.select("img");
+        Elements descAndGenres = allAnime.select("div.desc").select("i.fa.fa-info-circle.fa-lgg.long-description.html-popover");
+        Elements descAndGenres2 = descAndGenres.select("noindex");
+        Elements descAndGenres3 = descAndGenres2.select(".html-popover-holder");
 
+
+        for (Element element : imgAndTitle) {
+            System.out.print("Тайтл: ");
+            System.out.println(element.attr("title"));
+            System.out.print("Картинка тайтла: ");
+            System.out.println(element.attr("data-original"));
+            System.out.println("Жанры и описание тайтла:");
+            for (Element element2 : descAndGenres3) {
+                System.out.println(element2.getElementsByTag("span").text());
+            }
         }
+
 
     }
 }
