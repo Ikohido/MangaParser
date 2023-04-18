c9ec9fba76513ddbc009f0f866e0e6692dec6f72
Ikohido <rus.kanpaev@mail.ru>
2023-04-18 18:45:40 +0300

    Five

--- src/main/java/org/pars/Main.java
+++ src/main/java/org/pars/Main.java
@@ -15,21 +15,13 @@ public class Main {
                 .userAgent("Chrome/81.0.4044.138").get();
 
         Elements allAnime = document.getElementsByClass("col-sm-6");
-        Elements imgAndTitle = allAnime.select("img");
-        Elements descAndGenres = allAnime.select("div.desc").select("i.fa.fa-info-circle.fa-lgg.long-description.html-popover");
-        Elements descAndGenres2 = descAndGenres.select("noindex");
-        Elements descAndGenres3 = descAndGenres2.select(".html-popover-holder");
 
-
-        for (Element element : imgAndTitle) {
-            System.out.print("Тайтл: ");
-            System.out.println(element.attr("title"));
-            System.out.print("Картинка тайтла: ");
-            System.out.println(element.attr("data-original"));
-            System.out.println("Жанры и описание тайтла:");
-            for (Element element2 : descAndGenres3) {
-                System.out.println(element2.getElementsByTag("span").text());
-            }
+        for (Element element : allAnime) {
+            System.out.println("Тайтл: " + element.select("img").attr("title"));
+            System.out.println("Картинка тайтла: " + element.select("img").attr("data-original"));
+            System.out.println("Жанры тайтла: " + element.select(".badge.badge-light").text());
+            System.out.println("Описание тайтла: " + element.select(".manga-description").text());
+            System.out.println("");
         }
 
 
